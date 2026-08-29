3add0da748ad776d421b9f4a13edee21f2c2d61c
darylfragata <daryljf@protonmail.com>
2026-08-30 02:11:32 +0800

    fix: update bucket name variable

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -31,7 +31,7 @@ module "lambda_backend" {
 module "portfolio_site" {
   source = "../../modules/portfolio_site"
 
-  bucket_name  = local.portfolio_site.bucket_name
+  bucket_name  = var.s3_bucket_name
   environment  = var.environment
   project_name = var.project_name
 }
